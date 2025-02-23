88b45ca758870e6978d328c7d50935d0377eadb1
levinion <levinnion@gmail.com>
2025-02-23 14:49:53 +0800

    update

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -1,10 +1,10 @@
 function _tmux_init
-    nohup watchexec --workdir ~/.config/tmux/ tmux source-file ~/.config/tmux/tmux.conf >/dev/null 2>&1 &
+    #nohup watchexec --workdir ~/.config/tmux/ -w ~/.config/tmux/tmux.conf tmux source-file ~/.config/tmux/tmux.conf >/dev/null 2>&1 &
     tmux
 end
 
 function _tmux_cleanup
-    ps aux | rg watchexec | rg tmux | awk '{print $2}' | xargs kill
+    #ps aux | rg watchexec | rg tmux | awk '{print $2}' | xargs kill
     kill $fish_pid
 end
 
--- tmux/scripts/autoreload.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-watchexec --workdir ~/.config/tmux/ tmux source-file ~/.config/tmux/tmux.conf >/dev/null 2>&1
