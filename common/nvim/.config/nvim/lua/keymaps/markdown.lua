4878dedbe89bf254391d14d00c18598e0f5dff0b
levinion <levinnion@gmail.com>
2026-07-28 00:04:53 +0800

    replace markdown-preview with peek

--- common/nvim/.config/nvim/lua/keymaps/markdown.lua
+++ common/nvim/.config/nvim/lua/keymaps/markdown.lua
@@ -1,3 +1,10 @@
 vim.keymap.set("n", "<leader>ms", "<cmd>Markview splitToggle<CR>", { desc = "Toggle markview split" })
 vim.keymap.set("n", "<leader>mt", "<cmd>Markview toggle<CR>", { desc = "Toggle markview" })
-vim.keymap.set("n", "<leader>mb", "<cmd>MarkdownPreviewToggle<CR>", { desc = "Toggle markview view in browser" })
+vim.keymap.set("n", "<leader>mb", function()
+  local peek = require('peek')
+  if peek.is_open() then
+    peek.close()
+  else
+    peek.open()
+  end
+end, { desc = "Toggle markview view in browser" })
--- common/nvim/.config/nvim/lua/plugins/markdown.lua
+++ common/nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,28 +1,18 @@
 return {
-	{
-		"OXY2DEV/markview.nvim",
-		lazy = false,
-		dependencies = { "saghen/blink.cmp" },
-	},
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    dependencies = { "saghen/blink.cmp" },
+  },
 
-	-- install without yarn or npm
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
-
-	-- install with yarn or npm
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && yarn install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
-	},
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
 }
