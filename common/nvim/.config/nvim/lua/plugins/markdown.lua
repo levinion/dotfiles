be5d121daac86d56f5ab79b2b3f369e37987a27c
levinion <levinnion@gmail.com>
2026-07-28 00:17:14 +0800

    fix: open peek in browser rather than webview

--- common/nvim/.config/nvim/lua/plugins/markdown.lua
+++ common/nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,18 +1,20 @@
 return {
-  {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-    dependencies = { "saghen/blink.cmp" },
-  },
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+		dependencies = { "saghen/blink.cmp" },
+	},
 
-  {
-    "toppair/peek.nvim",
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
-    config = function()
-      require("peek").setup()
-      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
-  },
+	{
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup({
+				app = "browser",
+			})
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+		end,
+	},
 }
