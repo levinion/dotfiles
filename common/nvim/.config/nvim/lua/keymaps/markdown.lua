387a1848e1b1d992211ab36c5a75ca4eb4be93e2
levinion <levinnion@gmail.com>
2026-08-02 21:42:33 +0800

    change peek.nvim to markdown-preview.nvim

--- common/nvim/.config/nvim/lua/keymaps/markdown.lua
+++ common/nvim/.config/nvim/lua/keymaps/markdown.lua
@@ -1,8 +1,1 @@
-vim.keymap.set("n", "<leader>mb", function()
-	local peek = require("peek")
-	if peek.is_open() then
-		peek.close()
-	else
-		peek.open()
-	end
-end, { desc = "Toggle markview view in browser" })
+vim.keymap.set("n", "<leader>mb", "<cmd>MarkdownPreview<cr>", { desc = "Toggle markview view in browser" })
